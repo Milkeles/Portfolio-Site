3f2d4d803b3878a00186d28a806a970b0b854ccd
Hristo Hristov <milkeles.apps@gmail.com>
2024-09-12 22:38:22 +0300

    Updates

---
+++ scripts/navbar.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("exists");const e=document.getElementById("mobile-menu-button"),t=document.getElementById("mobile-menu"),s=()=>{t.classList.contains("hidden")?t.classList.remove("hidden"):t.classList.add("hidden")};e&&e.addEventListener("click",s)})();
