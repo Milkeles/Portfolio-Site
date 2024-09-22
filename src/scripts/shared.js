78ee1c9ea44a3873e097021277176972f9d1a391
Hristo Hristov <milkeles.apps@gmail.com>
2024-09-22 19:38:16 +0300

    Updated files to HTML5; Fixed Fatal Menu Bugs.

--- src/scripts/shared.js
+++ src/scripts/shared.js
@@ -1,6 +1,13 @@
+/*
+This script handles all the front-end logic that's shared across pages in the site:
+- Menu
+
+Author: Hristo Hristov (Milkeles)
+Date:   22/09/2024 (dd/mm/yyyy)
+*/
 "use strict";
 
-// MENU
+/*====== MENU ======*/
 const toggleMenuButton = document.getElementById("toggle-menu");
 const slideoverContainer = document.getElementById('slideover-container');
 const slideoverBg = document.getElementById('slideover-bg');
@@ -8,25 +15,53 @@ const slideover = document.getElementById('slideover');
 const horizontalMenu = document.getElementById("horizontal-menu");
 const dynamicNavigation = document.getElementById("mobile-navigation");
 
-// Handle scroll events
 let isOpen = false;
-const toggleSidebarMenu = () => {
-    toggleMenuButton.classList.toggle("hamburger-toggle");
-    dynamicNavigation.classList.toggle("md:hidden");
-    slideoverContainer.classList.toggle('invisible');
-    slideoverBg.classList.toggle('opacity-0');
-    slideoverBg.classList.toggle('opacity-50');
-    slideover.classList.toggle('translate-x-full');
-    slideover.classList.toggle('translate-x-0');
-    isOpen = !isOpen;
+
+// Make the dynamic menu visible or invisible.
+const toggleDynamicMenu = () => {
+    toggleMenuButton.classList.toggle('hamburger-toggle');
+
+    if (isOpen) {
+
+        // Ensure animations play before the element is removed from the DOM.
+        setTimeout(() => {
+            dynamicNavigation.classList.add('hidden');
+            slideoverContainer.classList.add('invisible');
+            toggleMenuButton.disabled = false;
+        }, 300);
+
+        toggleMenuButton.disabled = true;
+        slideoverBg.classList.add('opacity-0');
+        slideoverBg.classList.remove('opacity-50');
+        slideover.classList.add('translate-x-full');
+        slideover.classList.remove('translate-x-0');
+        slideover.classList.add('opacity-0');
+        slideover.classList.remove('opacity-100');
+        isOpen = false;
+    } else {
+        dynamicNavigation.classList.remove('hidden');
+        slideoverContainer.classList.remove('invisible');
+
+        // Ensure animations play after the element is added to the DOM.
+        requestAnimationFrame(() => {
+            slideoverBg.classList.remove('opacity-0');
+            slideoverBg.classList.add('opacity-50');
+            slideover.classList.remove('translate-x-full');
+            slideover.classList.add('translate-x-0');
+            slideover.classList.remove('opacity-0');
+            slideover.classList.add('opacity-100');
+        });
+
+        isOpen = true;
+    }
 };
 
 // Add click event listener for the mobile button
 if (toggleMenuButton) {
-    toggleMenuButton.addEventListener('click', toggleSidebarMenu);
+    toggleMenuButton.addEventListener('click', toggleDynamicMenu);
 }
 
-
+// Handle Scroll evenets
 const handleScroll = () => {
     const isSmallScreen = window.innerWidth <= 768;
 
@@ -40,28 +75,20 @@ const handleScroll = () => {
         } else {
             toggleMenuButton.classList.add('hidden');
             horizontalMenu.classList.remove('hidden');
+
+            // Close the dynamic menu when the user is on top of the page.
             if(isOpen) {
-                toggleMenuButton.classList.toggle("hamburger-toggle");
-                slideoverContainer.classList.toggle('invisible');
-                slideoverBg.classList.toggle('opacity-0');
-                slideoverBg.classList.toggle('opacity-50');
-                slideover.classList.toggle('translate-x-full');
-                slideover.classList.toggle('translate-x-0');
+                toggleDynamicMenu();
                 isOpen = false;
             }
-            
         }
     }
-
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Initial check to set the correct state
     handleScroll();
 
-    // Add scroll event listener
     window.addEventListener('scroll', handleScroll);
 
-    // Add resize event listener to handle screen size changes
     window.addEventListener('resize', handleScroll);
 });
